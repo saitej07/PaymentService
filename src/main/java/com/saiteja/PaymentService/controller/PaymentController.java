014c576ea713cdd02ba67564351fdfb4c9b88f08
saitej07 <saitejabollabathini@gmail.com>
2025-08-23 20:36:59 +0530

    Removed all security related things

--- src/main/java/com/saiteja/PaymentService/controller/PaymentController.java
+++ src/main/java/com/saiteja/PaymentService/controller/PaymentController.java
@@ -6,7 +6,6 @@ import com.saiteja.PaymentService.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,13 +15,11 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
-    //@PreAuthorize("hasAnyRole('Admin', 'Customer')")
     @PostMapping
     public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
         return new ResponseEntity<>(paymentService.doPayment(paymentRequest), HttpStatus.CREATED);
     }
 
-    //@PreAuthorize("hasAnyRole('Admin', 'Customer')")
     @GetMapping("/{orderId}")
     public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable String orderId) {
         return new ResponseEntity<>(paymentService.getPaymentDetailsByOrderId(orderId), HttpStatus.OK);
